34ba576b38272e557595522066246b681fc9cabd
Umang Jaiswal <mob.dev1-tc@uidai.net.in>
2026-07-03 11:58:43 +0530

    created ClfRepository.kt to handle Capture, Session and Resident usecases

---
+++ app/src/main/java/app/gov/uidai/contactlessregistration/repository/impl/ClfRepositoryImpl.kt
@@ -0,0 +1,50 @@
+package app.gov.uidai.contactlessregistration.repository.impl
+
+import app.gov.uidai.contactlessregistration.data.remote.api.ClfApiService
+import app.gov.uidai.contactlessregistration.data.remote.network.ApiResult
+import app.gov.uidai.contactlessregistration.data.remote.network.ResponseHandler
+import app.gov.uidai.contactlessregistration.model.capture.BatchCaptureRequest
+import app.gov.uidai.contactlessregistration.model.capture.CaptureRequest
+import app.gov.uidai.contactlessregistration.model.capture.CaptureResponse
+import app.gov.uidai.contactlessregistration.model.resident.ResidentLookupRequest
+import app.gov.uidai.contactlessregistration.model.resident.ResidentLookupResponse
+import app.gov.uidai.contactlessregistration.model.session.CloseSessionRequest
+import app.gov.uidai.contactlessregistration.model.session.CreateSessionRequest
+import app.gov.uidai.contactlessregistration.model.session.CreateSessionResponse
+import app.gov.uidai.contactlessregistration.repository.ClfRepository
+import javax.inject.Inject
+
+class ClfRepositoryImpl @Inject constructor(
+    private val apiService: ClfApiService
+) : ClfRepository {
+
+    override suspend fun lookupResident(
+        request: ResidentLookupRequest
+    ): ApiResult<ResidentLookupResponse> = ResponseHandler.safeApiCall {
+        apiService.lookupResident(request)
+    }
+
+    override suspend fun createSession(
+        request: CreateSessionRequest
+    ): ApiResult<CreateSessionResponse> = ResponseHandler.safeApiCall {
+        apiService.createSession(request)
+    }
+
+    override suspend fun closeSession(
+        request: CloseSessionRequest
+    ): ApiResult<Unit> = ResponseHandler.safeApiCall {
+        apiService.closeSession(request)
+    }
+
+    override suspend fun uploadCapture(
+        request: CaptureRequest
+    ): ApiResult<CaptureResponse> = ResponseHandler.safeApiCall {
+        apiService.uploadCapture(request)
+    }
+
+    override suspend fun uploadBatchCaptures(
+        request: BatchCaptureRequest
+    ): ApiResult<List<CaptureResponse>> = ResponseHandler.safeApiCall {
+        apiService.uploadBatchCaptures(request)
+    }
+}
