c9f43258c089c6cb1e426e178ee787ee4cd68d41
Umang Jaiswal <mob.dev1-tc@uidai.net.in>
2026-07-06 11:48:00 +0530

    demo number for testing

--- app/src/main/java/app/gov/uidai/contactlessregistration/model/resident/ResidentLookupRequest.kt
+++ app/src/main/java/app/gov/uidai/contactlessregistration/model/resident/ResidentLookupRequest.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class ResidentLookupRequest(
     @SerializedName("aadhaar_hash") val aadhaarHash: String,
-    @SerializedName("age_group") val ageGroup: String,
-    @SerializedName("gender") val gender: String,
-    @SerializedName("skin_tone") val skinTone: String
+    @SerializedName("age_group") val ageGroup: String = "25",
+    @SerializedName("gender") val gender: String = "Male",
+    @SerializedName("skin_tone") val skinTone: String = "Dusky"
 )
