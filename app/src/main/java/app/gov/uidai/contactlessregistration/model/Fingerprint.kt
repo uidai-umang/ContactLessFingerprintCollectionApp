b9ffa8ce102a805eeb0fce2f6f745cffb532a02f
Umang Jaiswal <mob.dev1-tc@uidai.net.in>
2026-07-15 13:02:06 +0530

    .

--- app/src/main/java/app/gov/uidai/contactlessregistration/model/Fingerprint.kt
+++ app/src/main/java/app/gov/uidai/contactlessregistration/model/Fingerprint.kt
@@ -15,7 +15,10 @@ class CLFingerprint(
     fingerQuality: FingerQuality? = null,
     val jp2ByteArray: ByteArray,
     val bitmap: Bitmap,
-    val imageBytes: ByteArray = jp2ByteArray
+    val imageBytes: ByteArray = jp2ByteArray,
+    val blurScore: Double = 0.0,
+    val brightnessScore: Double = 0.0,
+    val glareScore: Double = 0.0
 ) : Fingerprint(
     fingerPosition = fingerPosition,
     embedding = embedding,
@@ -28,14 +31,20 @@ class CLFingerprint(
         jp2ByteArray: ByteArray = this.jp2ByteArray,
         fingerQuality: FingerQuality? = this.fingerQuality,
         bitmap: Bitmap = this.bitmap,
-        imageBytes: ByteArray = this.imageBytes
+        imageBytes: ByteArray = this.imageBytes,
+        blurScore: Double = this.blurScore,
+        brightnessScore: Double = this.brightnessScore,
+        glareScore: Double = this.glareScore
     ): CLFingerprint = CLFingerprint(
         fingerPosition = fingerPosition,
         embedding = embedding,
         fingerQuality = fingerQuality,
         jp2ByteArray = jp2ByteArray,
         bitmap = bitmap,
-        imageBytes = imageBytes
+        imageBytes = imageBytes,
+        blurScore = blurScore,
+        brightnessScore = brightnessScore,
+        glareScore = glareScore
     )
 }
 
